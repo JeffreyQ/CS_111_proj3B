29a9eb1c55e740152015a086b32c51a1ec7dde79
Jeffrey Qiu <jeffreyyqiu@gmail.com>
2017-06-04 20:06:12 +0000

    Got rid of useless code

--- lab3b.py
+++ lab3b.py
@@ -82,23 +82,6 @@ import csv
 
 
 
-
-
-class Inode:
-	def __init__(self, inum, numLinks):
-		self.inum = inum # inode number
-		self.numLinks = numLinks # number of hard links to this inode
-		self.pointers = set() # the set of directory entries that point to this inode
-		self.blockptrs = set() # block pointers, EXT2: 12 direct, 3 indrect
-
-
-class Block:
-	def __init__(self, blockNum):
-		self.blockNum = blockNum # block number
-		self.pointers = set() # the set of inodes that point to this block
-
-
-
 class analyzer:
 	def __init__(self, csvfile):
 		# SUPERBLOCK SUMMARY
