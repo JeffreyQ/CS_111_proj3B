f0926caf376eb4157a758b9aee3545ec78dbd55c
Jeffrey Qiu <jeffreyyqiu@gmail.com>
2017-06-04 16:15:54 +0000

    added some comments + documentation

--- lab3b.py
+++ lab3b.py
@@ -1,6 +1,87 @@
 import sys
 import csv
 
+"""
+ * superblock summary
+ *
+ * A single new-line terminated line, comprised of eight comma-separated fields (with no white-space), summarizing the key file system parameters:
+ *
+ * 1. SUPERBLOCK
+ * 2. total number of blocks (decimal)
+ * 3. total number of i-nodes (decimal)
+ * 4. block size (in bytes, decimal)
+ * 5. i-node size (in bytes, decimal)
+ * 6. blocks per group (decimal)
+ * 7. i-nodes per group (decimal)
+ * 8.first non-reserved i-node (decimal)
+ *
+"""
+
+"""
+ * group summary
+ *
+ * Scan each of the groups in the file system. For each group, produce a new-line 
+ * terminated line for each group, each comprised of nine comma-separated fields 
+ * (with no white space), summarizing its contents.
+ *
+ * 1. GROUP
+ * 2. group number (decimal, starting from zero)
+ * 3. total number of blocks in this group (decimal)
+ * 4. total number of i-nodes in this group (decimal)
+ * 5. number of free blocks (decimal)
+ * 6. number of free i-nodes (decimal)
+ * 7. block number of free block bitmap for this group (decimal)
+ * 8. block number of free i-node bitmap for this group (decimal)
+ * 9. block number of first block of i-nodes in this group (decimal)
+ * 
+"""
+
+"""
+ * free block entries
+ *
+ * Scan the free block bitmap for each group. For each free block, produce a 
+ * new-line terminated line, with two comma-separated fields (with no white space).
+ * 
+ * 1. BFREE
+ * 2. number of the free block (decimal)
+ * 
+"""
+
+"""
+ * free I-node entries
+ *
+ * Scan the free I-node bitmap for each group. For each free I-node, produce a 
+ * new-line terminated line, with two comma-separated fields (with no white space).
+ * 
+ * 1. IFREE
+ * 2. number of the free I-node (decimal)
+ *
+"""
+
+"""
+ * I-node summary
+ *
+ * Scan the I-nodes for each group. For each valid (non-zero mode and non-zero link count) 
+ * I-node, produce a new-line terminated line, with 27 comma-separated fields (with no 
+ * white space). The first twelve fields are i-node attributes:
+ * 
+ * 1. INODE
+ * 2. inode number (decimal)
+ * 3. file type ('f' for file, 'd' for directory, 's' for symbolic link, '?" for anything else)
+ * 4. mode (octal)
+ * 5. owner (decimal)
+ * 6. group (decimal)
+ * 7. link count (decimal)
+ * 8. creation time (mm/dd/yy hh:mm:ss, GMT)
+ * 9. modification time (mm/dd/yy hh:mm:ss, GMT)
+ * 10. time of last access (mm/dd/yy hh:mm:ss, GMT)
+ * 11. file size (decimal)
+ * 12. number of blocks (decimal)
+ * 13. The next fifteen fields are block addresses (decimal, 12 direct, one indirect, one double indirect, one tripple indirect).
+"""
+
+
+
 
 
 class Inode:
@@ -46,11 +127,11 @@ class analyzer:
 		self.allocatedBlocks = set() # bad naming convention, intuitively refers to opposite of what the spec is trying to say
 		
 		self.reservedBlocks = set()
-
+		self.reservedInodes = set()
+		self.allocatedInodes = set()
 
 	def initData(self):
-		for i in range(1, 8):
-			self.reservedBlocks.add(i)
+		# refer to the summaries above
 		for row in self.reader:
 			if row[0] == "SUPERBLOCK":
 				self.numBlocks = row[1]
@@ -60,25 +141,34 @@ class analyzer:
 				self.blocksPerGroup = row[5]
 				self.inodesPerGroup = row[6]
 				self.firstNonRsrvdInode = row[7]
-			elif row[0] == "BFREE":
+			if row[0] == "BFREE":
 				self.free_blocks.add( int(row[1]) )	
-			elif row[0] == "IFREE":
+			if row[0] == "IFREE":
 				self.free_inodes.add( int(row[1]) )		
-			elif row[0] == "GROUP":
+			if row[0] == "GROUP":
 				self.groupNum = row[1]
 				self.numFreeBlocks = row[4]
 				self.numFreeInodes = row[5]
 				self.bbmapNum = row[6]
 				self.ibmapNum = row[7]
 				self.firstInodeBlockNum = row[8]
-			elif row[0] == "INODE":
+			if row[0] == "INODE":
+				# populate allocated inodes with inode
+				self.allocatedInodes.add( int(row[1]) )
+				# populate allocated blocks with inode direct pointers
 				for item in row[12:]: # slicing
 					if item == 0:
 						break
-					self.allocatedBlocks.add( int(item))
-			elif row[0] == "INDIRECT":
+					self.allocatedBlocks.add( int(item) )
+			# populate allocated blocks set with referenced blockNum of INDIR block
+			if row[0] == "INDIRECT":
 				self.allocatedBlocks.add( int(row[5]) )
-
+		# populate reservedBlocks set with blocks reserved by the system
+		for i in range(1, 8):
+			self.reservedBlocks.add(i)
+		# populate reservedInodes set with inodes reserved by the system
+		for i in range(1, self.firstNonRsrvdInode):
+			self.reservedInodes.add(i)
 	def printAllocatedBlocks(self):
 		for blockNum in self.allocatedBlocks:
 			if blockNum in self.free_blocks:
@@ -88,6 +178,12 @@ class analyzer:
 		for i in range(0, int(self.numBlocks)):
 			if i not in self.free_blocks and i not in self.allocatedBlocks and i not in self.reservedBlocks:
 				print "UNREFERENCED BLOCK %s" % (i)
+
+	def printAllocatedInodes(self):
+		for inodeNum in self.allocatedInodes:
+			if inodeNum in self.free_inodes:
+				print "hellohere %s" % (inodeNum)
+
 
 	def printContents(self):
 		for item in self.allocatedBlocks:
